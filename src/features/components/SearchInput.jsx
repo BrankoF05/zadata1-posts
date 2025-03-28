2dd0a517d3d5b76e2db449814a4c0ea8112dcec7
BrankoF05 <branko.fibinger1@gmail.com>
2025-03-28 09:12:40 +0100

    feat object destrukturiranje search input komponente

--- src/features/components/SearchInput.jsx
+++ src/features/components/SearchInput.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-function SearchInput(props) {
+function SearchInput({ setInput, setFilter, filter }) {
   function filterInput(e) {
-    props.setInput(e.target.value);
+    setInput(e.target.value);
   }
 
   function filterChange(e) {
-    props.setFilter(e.target.value);
+    setFilter(e.target.value);
   }
   return (
     <div>
-      <input name={props.filter} onChange={filterInput} />
+      <input name={filter} onChange={filterInput} />
       <select onChange={filterChange}>
         <option value="title">Filter by name</option>
         <option value="body">Filter by description</option>
